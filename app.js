469e2cc0a47fc69c73e24ec3c96574910b026440
Talles Corradini <tallescorradini@gmail.com>
2021-10-22 17:22:53 -0300

    feat: add max flying height

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const bird = document.querySelector(".bird");
 const gameDisplay = document.querySelector(".game-container");
+const sky = document.querySelector(".sky");
 const ground = document.querySelector(".ground");
 
 let position = 100;
 let gravity = 2;
+const JUMP_HEIGHT = 50;
+const MAX_FLYING_HEIGHT = sky.offsetHeight - bird.offsetHeight - JUMP_HEIGHT;
 
 function applyGravity(position) {
   return (position -= gravity);
@@ -15,7 +18,8 @@ function updatePosition(newPosition) {
 }
 
 function fly() {
-  updatePosition((position += 50));
+  console.log(sky.offsetHeight, bird.offsetHeight);
+  if (position < MAX_FLYING_HEIGHT) updatePosition((position += JUMP_HEIGHT));
 }
 document.addEventListener("keyup", fly);
 
