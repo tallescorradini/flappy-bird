67071f4659ffec02344343821d1019476f85e271
Talles Corradini <tallescorradini@gmail.com>
2021-10-23 12:51:43 -0300

    refactor: add factory functions for scene and bird

--- app.js
+++ app.js
@@ -1,37 +1,68 @@
-const bird = document.querySelector(".bird");
 const gameDisplay = document.querySelector(".game-container");
 const sky = document.querySelector(".sky");
 const ground = document.querySelector(".ground");
 
-let birdPosition = 100;
-let gravity = 2;
-const JUMP_HEIGHT = 50;
-const MAX_FLYING_HEIGHT = sky.offsetHeight - bird.offsetHeight - JUMP_HEIGHT;
 const KEY_CODE_SPACE_BAR = 32;
 
-function applyGravity(position) {
-  return (position -= gravity);
-}
+// GLOBAL
 
-function updatePosition(newPosition) {
-  birdPosition = newPosition;
-  bird.style.bottom = newPosition + "px";
+function makeScene() {
+  const SCENE_HEIGHT = sky.offsetHeight;
+  const GRAVITY = -2;
+
+  function applyGravity(entity) {
+    const entityPosition = entity.getPosition();
+    const newPosition = entityPosition + GRAVITY;
+    entity.updatePosition(newPosition);
+  }
+
+  function getMaxFlyingHeight(birdHeight, jumpHeight) {
+    return SCENE_HEIGHT - (birdHeight + jumpHeight);
+  }
+  return { getMaxFlyingHeight, applyGravity };
 }
+const scene = makeScene();
+
+function makeBird() {
+  const element = document.querySelector(".bird");
+  const INITIAL_POSITION = 100;
+  let position = INITIAL_POSITION;
+  const BIRD_HEIGHT = 45;
+  const JUMP_HEIGHT = 50;
+
+  function updatePosition(newPosition) {
+    position = newPosition;
+    element.style.bottom = newPosition + "px";
+  }
+
+  function fly() {
+    if (position < scene.getMaxFlyingHeight(BIRD_HEIGHT, JUMP_HEIGHT))
+      updatePosition(position + JUMP_HEIGHT);
+  }
+
+  return {
+    getElement: () => element,
+    getPosition: () => position,
+    fly,
+    updatePosition,
+  };
+}
+const bird = makeBird();
 
 function controls(e) {
-  if (e.keyCode === KEY_CODE_SPACE_BAR) fly();
+  if (e.keyCode === KEY_CODE_SPACE_BAR) bird.fly();
 }
 
-function fly() {
-  if (birdPosition < MAX_FLYING_HEIGHT)
-    updatePosition((birdPosition += JUMP_HEIGHT));
+function updatePositionX(element, position) {
+  element.style.left = position + "px";
 }
+
 document.addEventListener("keyup", controls);
 
 function startGame() {
-  updatePosition(applyGravity(birdPosition));
+  scene.applyGravity(bird);
 }
-let gameTimerId = setInterval(startGame, 20);
+let gameTimerId = setInterval(startGame, 200);
 
 const STARTING_POSITION = 500;
 
@@ -43,24 +74,18 @@ function generateObstacle() {
   const randomHeight = 50 + Math.floor(Math.random() * 250);
   obstacle.style.height = randomHeight + "px";
 
-  function updateObstaclePosition(xPosition) {
-    obstacle.style.left = xPosition + "px";
-  }
-
   let xPosition = STARTING_POSITION;
-  updateObstaclePosition(xPosition);
+  updatePositionX(obstacle, xPosition);
 
   function moveObstacle() {
     xPosition -= 2;
 
-    updateObstaclePosition(xPosition);
+    updatePositionX(obstacle, xPosition);
 
     if (xPosition < -60) {
       clearInterval(timerId);
       sky.removeChild(obstacle);
     }
-
-    if (birdPosition <= 0) gameOver();
   }
   let timerId = setInterval(moveObstacle, 20);
 
